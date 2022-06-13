e1c3146e482ddf64d5b926a0595731de193adf45
juancr15 <juan288cruz@gmail.com>
2022-06-12 19:20:27 -0500

    punto_1 updated

---
+++ python-fundamentals/desafios/iniciando_viaje/punto_1.py
@@ -0,0 +1,29 @@
+'''R2D2 o Arturito es uno de los robots más famosos de la galaxia, 
+   debes escribir un programa que lo dibuje en segunda dimensión, 
+   como se presenta en la siguiente imagen:'''
+
+print("       _____")
+print("     /   ()  \\")
+print("  _ | _______ | _")
+print("|   |  ====   |   |")
+print("| _ |    o    | _ |")
+print("  | |    o    | |")
+print("  | | ___ ___ | |")
+print(" |~  \\ _____ / ~ |")
+print(" /=\\    /=\\    /=\\")
+print(" [ ]    [ ]    [ ]")
+
+
+'''
+    Option 2 to get the same result
+'''
+print("       _____\n\
+     /   ()  \\\n\
+  _ | _______ | _\n\
+|   |  ====   |   |\n\
+| _ |    o    | _ |\n\
+  | |    o    | |\n\
+  | | ___ ___ | |\n\
+ |~  \\ _____ / ~ |\n\
+ /=\\    /=\\    /=\\\n\
+ [ ]    [ ]    [ ]")
