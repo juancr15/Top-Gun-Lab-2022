6b66c3500f3b830d3f91a675eb27b63da507b64d
juancr15 <juan288cruz@gmail.com>
2022-06-15 08:32:48 -0500

    Punto 1 caminos repetidos

--- python-fundamentals/desafios/caminos_repetitivos/punto_1.py
+++ python-fundamentals/desafios/caminos_repetitivos/punto_1.py
@@ -4,3 +4,38 @@
     para que sirva de vigía en el mástil principal.
 '''
 
+# Dictionary of creatures and their respective articles
+dictCreatures = {'Kraken': 'un', 'Sirenas': 'unas', 
+                'Ballena': 'una', 'Hipocampo': 'un', 
+                'Macaraprono': 'una', 'Pulpo': 'un', 
+                'Leviatanes': 'unos', 'Hidras': 'unas'}
+
+# Dictionary of the ship steering and its preposition
+dictShipSteering = {'babor': 'a', 'estribor': 'a', 'proa': 'por la', 'popa': 'por la'}
+
+# Initialize variables
+creature = ''
+
+
+while creature != 'Stop':
+
+    # Asking for the creature
+    creature = input('¿Qué criatura estás viendo?\n\t')
+
+    # Validating the if the creature is in the dictionary
+    if creature.capitalize() in dictCreatures:
+        ship_steering = input(f'¿Dónde estás viendo {dictCreatures[creature.capitalize()]} {creature.capitalize()}?\n\t')
+
+        # Validating the if the ship steering is in the dictionary
+        if ship_steering.lower() not in dictShipSteering:
+            print('No reconozco esa dirección. Intentalo nuevamente.')
+            continue
+        print(f'¡Ahoy! capitán, {dictCreatures[creature.capitalize()]} {creature.capitalize()} {dictShipSteering[ship_steering.lower()]} {ship_steering.lower()}.\n')
+    
+    # Stopping the loop if the creature is 'Stop'
+    elif creature.capitalize() == 'Stop':
+        print('¡Hasta aquí llegamos, marinero! Buen viaje.')
+        break
+    else:
+        print('Esa criatura no existe. Ingresala nuevamente!')
+        continue
